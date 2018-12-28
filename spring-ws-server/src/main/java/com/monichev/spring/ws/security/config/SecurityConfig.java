99bcf24eb238ee9555ee2600de391ee52d77bfe7
Sergey Monichev <sergey.monichev@gmail.com>
2018-12-28 22:36:27 +0300

    Cleanup

--- spring-ws-server/src/main/java/com/monichev/spring/ws/security/config/SecurityConfig.java
+++ spring-ws-server/src/main/java/com/monichev/spring/ws/security/config/SecurityConfig.java
@@ -7,17 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -35,14 +29,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-                .authorizeRequests().anyRequest().authenticated()
+            .authorizeRequests().anyRequest().authenticated()
             .and()
-                .csrf().disable()
-                .formLogin().disable()
-                .logout().disable()
-                .httpBasic().realmName(BasicAuthenticationEntryPoint.REALM_NAME).authenticationEntryPoint(entryPoint);
+            .csrf().disable()
+            .formLogin().disable()
+            .logout().disable()
+            .httpBasic().realmName(BasicAuthenticationEntryPoint.REALM_NAME).authenticationEntryPoint(entryPoint);
     }
 
     @Override
