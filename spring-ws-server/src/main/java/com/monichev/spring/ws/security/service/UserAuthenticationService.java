c32a641589bd1c93e890d9f90e95079d9b9aa58b
Sergey Monichev <sergey.monichev@gmail.com>
2019-01-14 12:36:47 +0300

    Cleanup

--- spring-ws-server/src/main/java/com/monichev/spring/ws/security/service/UserAuthenticationService.java
+++ spring-ws-server/src/main/java/com/monichev/spring/ws/security/service/UserAuthenticationService.java
@@ -2,15 +2,7 @@ package com.monichev.spring.ws.security.service;
 
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-
 import com.monichev.spring.ws.security.entity.User;
-
-import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 public interface UserAuthenticationService {
 
