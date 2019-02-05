6aeefc2110b1a9e7f12081cf39e1c5fcc2bb965a
Sergey Monichev <sergey.monichev@gmail.com>
2019-02-06 01:25:43 +0300

    Switch to Jackson Json Provider

--- spring-ws-server/src/main/java/com/monichev/spring/ws/config/RestServiceConfig.java
+++ spring-ws-server/src/main/java/com/monichev/spring/ws/config/RestServiceConfig.java
@@ -3,6 +3,7 @@ package com.monichev.spring.ws.config;
 import java.util.Collections;
 import java.util.List;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.monichev.spring.ws.service.ApiService;
 
 import org.apache.cxf.Bus;
@@ -14,13 +15,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class RestServiceConfig {
+
     @Bean
-    public Server restServer(List<ApiService> apiServiceList, Bus bus) {
+    public JacksonJsonProvider jacksonJsonProvider() {
+        return new JacksonJsonProvider();
+    }
+
+    @Bean
+    public Server restServer(List<ApiService> apiServiceList, Bus bus, JacksonJsonProvider jacksonJsonProvider) {
         JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
         endpoint.setBus(bus);
         endpoint.setAddress("/spring-rs");
         endpoint.setServiceBeans((List<Object>)(List<?>) apiServiceList);
         endpoint.setFeatures(Collections.singletonList(new Swagger2Feature()));
+        endpoint.setProvider(jacksonJsonProvider);
         return endpoint.create();
     }
 }
